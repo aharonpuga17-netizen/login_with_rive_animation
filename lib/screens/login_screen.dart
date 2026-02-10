98e8636fa4e0322fcdbad24204d25fd609e69e9e
aharonpuga17-netizen <aharonpuga17@gmail.com>
2026-02-10 13:29:55 -0600

    feat(login):add email and password inputs

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -9,19 +9,68 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+
+  // Control para mostrar/ocultar la contraseña
+  bool _obscureText = true;
+
   @override
   Widget build(BuildContext context) {
+    // Para obtener el tamaño de la pantalla
+    final Size size = MediaQuery.of(context).size;
+
     return Scaffold(
       body: SafeArea(
         // Evita que el contenido se superponga con el notch
-        child: Column(
-          children: [
-            Expanded(
-              child: RiveAnimation.asset(
-                'assets/animated_login_bear.riv',
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: Column(
+            children: [
+              SizedBox(
+                width:size.width,
+                height:200,
+                child: RiveAnimation.asset('assets/animated_login_bear.riv',)
+                ),
+              // Para separación
+              const SizedBox(height: 10),
+
+              TextField(
+                decoration: InputDecoration(
+                  hintText: 'Email',
+                  prefixIcon: const Icon(Icons.email),
+                  border: OutlineInputBorder(
+                    // Para redondear los bordes
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
               ),
-            ),
-          ],
+
+              const SizedBox(height: 10),
+
+              TextField(
+                obscureText: _obscureText,
+                decoration: InputDecoration(
+                  hintText: 'Password',
+                  prefixIcon: const Icon(Icons.lock),
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      _obscureText
+                          ? Icons.visibility_off
+                          : Icons.visibility,
+                    ),
+                    onPressed: () {
+                      //Refresca el icono
+                      setState(() {
+                        _obscureText = !_obscureText;
+                      });
+                    },
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
